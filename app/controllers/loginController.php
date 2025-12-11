2678b65ed4365f446ba6b1e9337711498472598c
Juan Mahecha <mahechajuanda29@gmail.com>
2025-12-11 14:25:41 -0500

    solucione problemas para entrar al administrador

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
             break;
     }
 
-    mostrarSweetAlert('success', 'Ingreso exitoso', $mensaje, $rendirectUrl);
+    mostrarSweetAlert('success', 'Ingreso exitoso', $mensaje, $redirectUrl);
     exit();
 
 } else{
