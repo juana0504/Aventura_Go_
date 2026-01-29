90910b11056bab3df9bad1b3ec45625434f0a0be
albert <alberix252@gmail.com>
2026-01-28 22:57:59 -0500

    se hizo el Registro de cambios de contraseña, cuándo un usuario cambia su clave se  Insertar evento en log quedando ok la tarea 12.2.3 [BACKEND] del trello.

--- app/controllers/passwordChangeController.php
+++ app/controllers/passwordChangeController.php
@@ -27,7 +27,7 @@ class PasswordChangeController
         $model = new PasswordChange();
 
         // Obtener clave actual
-        $usuario = $model->obtenerClaveActual($_SESSION['user']['id']);
+        $usuario = $model->obtenerClaveActual($_SESSION['user']['id_usuario']);
 
         if (!$usuario || !password_verify($clave_actual, $usuario['clave'])) {
             mostrarSweetAlert('error', 'Error', 'La contraseña actual es incorrecta.');
@@ -37,8 +37,30 @@ class PasswordChangeController
         // Actualizar
         $nueva_hash = password_hash($clave_nueva, PASSWORD_DEFAULT);
 
-        if ($model->actualizarClave($_SESSION['user']['id'], $nueva_hash)) {
-            mostrarSweetAlert('success', 'Listo', 'Contraseña actualizada correctamente.', '/aventura_go/login');
+        // REGISTRO DE CAMBIO DE CONTRASEÑA (LOG)
+        require_once BASE_PATH . '/config/database.php';
+        $db = new conexion();
+        $conexion = $db->getConexion();
+
+        if ($model->actualizarClave($_SESSION['user']['id_usuario'], $nueva_hash)) {
+
+            $id_usuario = $_SESSION['user']['id_usuario']; // confirma que este índice existe
+            $ip = $_SERVER['REMOTE_ADDR'];
+
+            $sqlLog = "INSERT INTO log_cambio_contrasena (id_usuario, ip)
+           VALUES (:id_usuario, :ip)";
+
+            $stmtLog = $conexion->prepare($sqlLog);
+            $stmtLog->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+            $stmtLog->bindParam(':ip', $ip, PDO::PARAM_STR);
+            $stmtLog->execute();
+
+            mostrarSweetAlert(
+                'success',
+                'Listo',
+                'Contraseña actualizada correctamente.',
+                '/aventura_go/login'
+            );
         } else {
             mostrarSweetAlert('error', 'Error', 'No se pudo actualizar la contraseña.');
         }
