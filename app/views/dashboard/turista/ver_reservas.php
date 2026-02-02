95abd1b393f22a53a3e5dda8ee613c2760eb4300
sofia camelo <camelojimenezsofia20@gmail.com>
2026-02-02 16:14:54 -0500

    cambie el id que tenia por id_usuario

--- app/views/dashboard/turista/ver_reservas.php
+++ app/views/dashboard/turista/ver_reservas.php
@@ -55,7 +55,7 @@ require_once BASE_PATH . '/app/helpers/session_turista.php';
                     <i class="bi bi-clock"></i> Pendientes
                 </button>
 
-                <a href="<?= BASE_URL ?>/proveedor/pdf-actividades" class="btn-pdf" target="_blank">
+                <a href="<?= BASE_URL ?>/turista/pdf-actividades" class="btn-pdf" target="_blank">
                     <i class="bi bi-file-earmark-pdf"></i>Generar Reportes
                 </a>
 
