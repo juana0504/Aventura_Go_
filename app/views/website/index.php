f79aac562cae98fa2a405fed0ff9c13321f6f1be
albert <alberix252@gmail.com>
2026-03-11 21:45:33 -0500

    en index.php se arreglo  y se dejo limpiio para q quedara acorde con el dominio

--- app/views/website/index.php
+++ app/views/website/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aventura Go - contactanos</title>
+    <title>Aventura Go</title>
 
     <link rel="icon" type="image/png" href="<?= BASE_URL ?>public/assets/website_externos/index/img/FAVICON.png">
 
